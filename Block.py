a3dd0316e228de57b8f1b10b79f7489c404ac946
BassamMattar <bassammattar2099@gmail.com>
2020-05-02 14:55:16 +0200

    Block implementation.

--- Block.py
+++ Block.py
@@ -1,3 +1,7 @@
+import json
+from hashlib import sha256
+
+
 class Block:
     def __init__(self, index, transactions, timestamp, previous_hash, nonce=0):
         """
@@ -6,24 +10,51 @@ class Block:
             * prev block hash
             * merkle tree hash
             * nonce
+            * index
             Block body should contain
             * transactions
             * transactions length
             * block index
         """
         # Block header
-        # Todo
-
+        self.timestamp = timestamp
+        self.previous_hash = previous_hash
+        self.merkle_tree_hash = self.get_merkle_tree_hash(transactions)
+        self.nonce = nonce
+        self.index = index
         # Block body
-
-        # Todo
-        pass
+        self.transactions = transactions
+        self.transactions_length = len(transactions)
 
     def compute_hash(self):
-        """
-            A function that return the hash of the block contents.
-            return sha256 hash string
-        """
-        # Todo
-        pass
+        block_string = json.dumps(self.__dict__, sort_keys=True)
+        return sha256(block_string.encode()).hexdigest()
+
+    def get_merkle_tree_hash(self, transactions):
+        if len(transactions) == 0:
+            return False
+        # get hash of each transaction
+        transactions_hashes = []
+        for transaction in transactions:
+            transaction_string = json.dumps(transaction.__dict__, sort_keys=True)
+            transactions_hash = sha256(transaction_string.encode()).hexdigest()
+            transactions_hashes.append(transactions_hash)
+
+        transactions_hashes.append("Marker")
+        while True:
+            t1 = transactions_hashes.pop(0)
+            if str(t1) == "Marker":
+                t1 = transactions_hashes.pop(0)
+                if len(transactions_hashes) == 0:
+                    return t1
+                transactions_hashes.append("Marker")
+            t2 = transactions_hashes.pop(0)
+            if str(t2) == "Marker":
+                if len(transactions_hashes) == 0:
+                    return t1
+                t2 = t1
+                transactions_hashes.insert(0, "Marker")
+            combine_t1_t2_hash = sha256(str(t1).encode() + str(t2).encode()).hexdigest()
+            transactions_hashes.append(combine_t1_t2_hash)
+
 
